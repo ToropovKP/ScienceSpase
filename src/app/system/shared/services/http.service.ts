8501e65ceff6f52a78bf7b067945b85dd03b89cb
Daniil Moiseev <daniil.moiseev@qligent.com>
2025-01-07 23:53:43 +0300

    Update http service

--- src/app/system/shared/services/http.service.ts
+++ src/app/system/shared/services/http.service.ts
@@ -27,6 +27,8 @@ export class HttpService {
   constructor(private http: HttpClient) {
   }
 
+  // /auth
+
   private updateHeaders() {
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${sessionStorage.getItem('token')}`);
   }
@@ -40,141 +42,140 @@ export class HttpService {
     return await firstValueFrom(this.http.post<void>(`${baseUrl}/api/v1/auth/logout`, '', this.httpOptions));
   }
 
+  // /user
+
   async registration(request: object): Promise<User> {
-    return await firstValueFrom(this.http.post<User>(`${baseUrl}/api/v1/auth/registration`, request, this.httpOptions));
-  }
-
-  async getModerators(): Promise<UserBase[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<UserBase[]>(`${baseUrl}/api/v1/admin/user/getModerators`, this.httpOptions));
-  }
-
-  async getModeratorsByConference(conferenceId: string): Promise<UserBase[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<UserBase[]>(`${baseUrl}/api/v1/admin/conference/${conferenceId}/getModerators`, this.httpOptions));
-  }
-
-  async getReviewers(): Promise<UserBase[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<UserBase[]>(`${baseUrl}/api/v1/admin/user/getReviewers`, this.httpOptions));
+    return await firstValueFrom(this.http.post<User>(`${baseUrl}/api/v1/user/create`, request, this.httpOptions));
   }
 
   async getUsers(): Promise<User[]> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.get<User[]>(`${baseUrl}/api/v1/admin/getAllUsers`, this.httpOptions));
+    return await firstValueFrom(this.http.get<User[]>(`${baseUrl}/api/v1/user/all`, this.httpOptions));
   }
 
-  async getUserInfo(email: string): Promise<User> {
+  async getModerators(): Promise<UserBase[]> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.get<User>(`${baseUrl}/api/v1/member/getUser?email=${email}`, this.httpOptions));
+    return await firstValueFrom(this.http.get<UserBase[]>(`${baseUrl}/api/v1/user/moderators`, this.httpOptions));
+  }
+
+  async getReviewers(): Promise<UserBase[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<UserBase[]>(`${baseUrl}/api/v1/user/reviewers`, this.httpOptions));
   }
 
   async getUserInfoById(id: string): Promise<User> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.get<User>(`${baseUrl}/api/v1/member/getUserById?userId=${id}`, this.httpOptions));
+    return await firstValueFrom(this.http.get<User>(`${baseUrl}/api/v1/user?id=${id}`, this.httpOptions));
   }
 
-  async updateUserInfo(request: object): Promise<User> {
+  async getUserInfo(email: string): Promise<User> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.put<User>(`${baseUrl}/api/v1/member/profile/update`, JSON.stringify(request), this.httpOptions));
-  }
-
-  async updateUserInfoByJob(request: object): Promise<User> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.put<User>(`${baseUrl}/api/v1/member/profile/update?job=true`, JSON.stringify(request), this.httpOptions));
-  }
-
-  async createJob(request: object): Promise<Job> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.post<Job>(`${baseUrl}/api/v1/member/jobs`, JSON.stringify(request), this.httpOptions));
-  }
-
-  async deleteJob(id: string): Promise<Job> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.delete<Job>(`${baseUrl}/api/v1/member/job/${id}/delete`, this.httpOptions));
-  }
-
-  async reviewJob(id: string, request: ReviewDto): Promise<void> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.post<void>(`${baseUrl}/api/v1/member/job/${id}/review`, request, this.httpOptions));
+    return await firstValueFrom(this.http.get<User>(`${baseUrl}/api/v1/user?email=${email}`, this.httpOptions));
   }
 
   async changeUserRole(id: string, role: string): Promise<boolean> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.post<boolean>(`${baseUrl}/api/v1/admin/user/appointrole?userId=${id}&role=${role}`, {}, this.httpOptions));
+    return await firstValueFrom(this.http.post<boolean>(`${baseUrl}/api/v1/user/appointrole?id=${id}&role=${role}`, {}, this.httpOptions));
   }
 
   async changeUserStatus(id: string, status: string): Promise<boolean> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.post<boolean>(`${baseUrl}/api/v1/admin/user/changestatus?userId=${id}&status=${status}`, {}, this.httpOptions));
+    return await firstValueFrom(this.http.post<boolean>(`${baseUrl}/api/v1/user/changestatus?id=${id}&status=${status}`, {}, this.httpOptions));
   }
 
-  async getConferenceUsers(id: string): Promise<number> {
+  async updateUserInfo(request: object): Promise<User> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.get<number>(`${baseUrl}/api/v1/admin/conference/${id}/users`, this.httpOptions));
+    return await firstValueFrom(this.http.put<User>(`${baseUrl}/api/v1/user/profile/update`, JSON.stringify(request), this.httpOptions));
   }
 
-  async getConferenceJobs(id: string): Promise<Job[]> {
+  async updateUserInfoByJob(request: object): Promise<User> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.get<Job[]>(`${baseUrl}/api/v1/admin/jobs/${id}`, this.httpOptions));
+    return await firstValueFrom(this.http.put<User>(`${baseUrl}/api/v1/user/profile/update?job=true`, JSON.stringify(request), this.httpOptions));
   }
 
-  async getUserJobs(id: string): Promise<Job[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Job[]>(`${baseUrl}/api/v1/member/jobs/${id}`, this.httpOptions));
-  }
-
-  async getJobComments(id: string): Promise<Comment[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Comment[]>(`${baseUrl}/api/v1/member/comments/${id}`, this.httpOptions));
-  }
-
-  async getUserOneJob(id: string): Promise<Job> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Job>(`${baseUrl}/api/v1/member/job/${id}`, this.httpOptions));
-  }
-
-  async getConferences(): Promise<Conference[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Conference[]>(`${baseUrl}/api/v1/member/conferences`, this.httpOptions));
-  }
-
-  async getConference(id: string): Promise<Conference> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Conference>(`${baseUrl}/api/v1/member/conference/${id}`, this.httpOptions));
-  }
-
-  async getSections(id: string): Promise<Section[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<Section[]>(`${baseUrl}/api/v1/member/conference/${id}/sections`, this.httpOptions));
-  }
-
-  async getTags(): Promise<string[]> {
-    this.updateHeaders();
-    return await firstValueFrom(this.http.get<string[]>(`${baseUrl}/api/v1/member/conference/tags`, this.httpOptions));
-  }
+  // /conference
 
   async createConference(request: object): Promise<Conference> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.post<Conference>(`${baseUrl}/api/v1/admin/conference/create`, JSON.stringify(request), this.httpOptions));
+    return await firstValueFrom(this.http.post<Conference>(`${baseUrl}/api/v1/conference/create`, JSON.stringify(request), this.httpOptions));
   }
 
   async updateConference(id: string, request: object): Promise<Conference> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.put<Conference>(`${baseUrl}/api/v1/admin/conference/${id}/update`, JSON.stringify(request), this.httpOptions));
+    return await firstValueFrom(this.http.put<Conference>(`${baseUrl}/api/v1/conference/update?id=${id}`, JSON.stringify(request), this.httpOptions));
+  }
+
+  async getConferenceUsers(id: string): Promise<number> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<number>(`${baseUrl}/api/v1/conference/users?id=${id}`, this.httpOptions));
   }
 
   async appointModeratorToConference(id: string, admins: UserBaseDto[]): Promise<void> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.post<void>(`${baseUrl}/api/v1/admin/conference/${id}/appointmoderator`, JSON.stringify(admins), this.httpOptions));
+    return await firstValueFrom(this.http.post<void>(`${baseUrl}/api/v1/conference/appointmoderator?id=${id}`, JSON.stringify(admins), this.httpOptions));
   }
 
-  async uploadFile(formData: FormData): Promise<UploadResponse> {
+  async getConference(id: string): Promise<Conference> {
     this.updateHeaders();
-    return await firstValueFrom(this.http.post<UploadResponse>(`${baseUrl}/api/v1/files/uploadFile`, formData));
+    return await firstValueFrom(this.http.get<Conference>(`${baseUrl}/api/v1/conference?id=${id}`, this.httpOptions));
   }
 
-  //conferenceId: string, sectionId: string,
+  async getConferences(): Promise<Conference[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Conference[]>(`${baseUrl}/api/v1/conference/all`, this.httpOptions));
+  }
+
+  async getSections(id: string): Promise<Section[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Section[]>(`${baseUrl}/api/v1/conference/sections?id=${id}`, this.httpOptions));
+  }
+
+  async getTags(): Promise<string[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<string[]>(`${baseUrl}/api/v1/conference/tags`, this.httpOptions));
+  }
+
+  // /job
+
+  async createJob(request: object): Promise<Job> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.post<Job>(`${baseUrl}/api/v1/job`, JSON.stringify(request), this.httpOptions));
+  }
+
+  async getUserOneJob(id: string): Promise<Job> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Job>(`${baseUrl}/api/v1/job?id=${id}`, this.httpOptions));
+  }
+
+  async getConferenceJobs(id: string): Promise<Job[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Job[]>(`${baseUrl}/api/v1/job/all?conferenceId=${id}`, this.httpOptions));
+  }
+
+  async getUserJobs(id: string): Promise<Job[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Job[]>(`${baseUrl}/api/v1/job/all?userId=${id}`, this.httpOptions));
+  }
+
+  async deleteJob(id: string): Promise<Job> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.delete<Job>(`${baseUrl}/api/v1/job/delete?id=${id}`, this.httpOptions));
+  }
+
+  async reviewJob(id: string, request: ReviewDto): Promise<void> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.post<void>(`${baseUrl}/api/v1/job/review?id=${id}`, request, this.httpOptions));
+  }
+
+  // /comment
+
+  async getJobComments(id: string): Promise<Comment[]> {
+    this.updateHeaders();
+    return await firstValueFrom(this.http.get<Comment[]>(`${baseUrl}/api/v1/chat?id=${id}`, this.httpOptions));
+  }
+
+  // /files
+
   async uploadFiles(formData: FormData): Promise<UploadResponse[]> {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${sessionStorage.getItem('token')}`, // Добавляем токен, если нужен
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,12 +45,11 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
 (window as any).global = window;
-(window as any).Buffer = (window as any).Buffer || require('buffer').Buffer;
-(window as any).process = require('process');
